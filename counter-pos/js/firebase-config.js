4c34f755911dda82c1d5524a112972e85575ed10
nolegoafrica <nolegoafrica@gmail.com>
2026-08-02 02:13:55 +0400

    Add files via upload

---
+++ counter-pos/js/firebase-config.js
@@ -0,0 +1,14 @@
+// This file connects Counter POS to your Firebase project.
+// Keep this file only on your own computer / private repo if possible.
+ 
+const firebaseConfig = {
+  apiKey: "AIzaSyDa6mfFeSC6EVr4cuqbp35vRMoZ4csdUJM",
+  authDomain: "counterposnaluno.firebaseapp.com",
+  projectId: "counterposnaluno",
+  storageBucket: "counterposnaluno.firebasestorage.app",
+  messagingSenderId: "236715196702",
+  appId: "1:236715196702:web:58a8241248f9c061adc90f"
+};
+ 
+firebase.initializeApp(firebaseConfig);
+ 
